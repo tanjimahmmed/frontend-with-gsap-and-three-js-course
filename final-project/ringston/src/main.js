6b05b87e49e8d03b7e203a7e73136972dd04efd2
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-03-17 13:06:33 +0600

    add inspection section animation

--- final-project/ringston/src/main.js
+++ final-project/ringston/src/main.js
@@ -137,6 +137,35 @@ function animateWords() {
     setInterval(updateText, 3000)
 }
 
+function inspectionSection() {
+    const inspectionTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.inspection',
+            start: 'top bottom',
+            end: 'bottom center',
+            scrub: true
+        }
+    })
+    inspectionTl
+    .to('.inspection h2', {
+        y: -100
+    })
+    .to('.ring-bg', {
+        y: -50,
+        height: 300
+    }, '<')
+
+    gsap.to('.marquee h3', {
+        scrollTrigger: {
+            trigger: '.marquee h3',
+            start: 'top 80%',
+            end: 'bottom top',
+            scrub: true
+        },
+        x: 200
+    })
+}
+
 
 function initRenderLoop() {
 
@@ -186,8 +215,9 @@ function setupSmoothScroll() {
 document.addEventListener('DOMContentLoaded', () => {
     initThreeJS();
     initRenderLoop();
-    
+
     animateWords();
+    inspectionSection();
     
 
     setupSmoothScroll();
